c4c03cbda61158ae6a02de36f9fef0445db5f74a
Manish Hyongoju <m.hyongoju04@gmail.com>
2024-11-23 10:31:16 +0545

    feat: image zoom in and zoom out effect

--- src/components/categories/category/categoryCardWithImageZoomIn.tsx
+++ src/components/categories/category/categoryCardWithImageZoomIn.tsx
@@ -8,19 +8,19 @@ import { ExpandSvg } from '@/assets/svgs';
 import ShrinkIcon from '@/assets/svgs/ShrinkIcon';
 
 export default function CategoryCardWithImageZoomIn({ className }: { className?: string }) {
-    const { isImageZoomIn, setIsImageZoomIn } = useCategoryImage();
+  const { isImageZoomIn, setIsImageZoomIn } = useCategoryImage();
 
-    return (
-        <div className="relative flex place-items-center  overflow-hidden aspect-square object-cover rounded-md ">
-            <CategoriesCard
-                className={cn('transition-transform duration-300 w-full h-full', {
-                    'scale-125': isImageZoomIn,
-                    'scale-100': !isImageZoomIn,
-                })}
-            />
-            <Button onClick={setIsImageZoomIn} className="absolute bottom-4 right-4 bg-foreground p-2 h-10 w-10 rounded-lg shadow-md bg-opacity-50">
-                {isImageZoomIn ? <ShrinkIcon /> : <ExpandSvg />}
-            </Button>
-        </div>
-    );
+  return (
+    <div className="relative flex place-items-center  overflow-hidden aspect-square object-cover rounded-md ">
+      <CategoriesCard
+        className={cn('transition-transform duration-300 w-full h-full', {
+          'scale-125': isImageZoomIn,
+          'scale-100': !isImageZoomIn,
+        })}
+      />
+      <Button onClick={setIsImageZoomIn} className="absolute bottom-4 right-4 bg-foreground p-2 h-10 w-10 rounded-lg shadow-md bg-opacity-50">
+        {isImageZoomIn ? <ShrinkIcon /> : <ExpandSvg />}
+      </Button>
+    </div>
+  );
 }
