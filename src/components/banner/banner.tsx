351e88835e08514207360b858bbb05ab61ecbc91
Manish Hyongoju <m.hyongoju04@gmail.com>
2024-11-24 06:23:25 +0545

    feat: added link to banner shop now

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -7,9 +7,7 @@ export default function Banner() {
   return (
     <Card className="flex flex-col justify-end" colorScheme={'banner-card'} rounded={'16'} padding={'48px'}>
       <div className="w-1/2 space-y-4">
-        <p className="flex-wrap text-[30px] lg:text-[40px] font-medium text-text-primary leading-tight">
-          Grab Upto 50% Off On Selected Clothes
-        </p>
+        <p className="flex-wrap text-[30px] lg:text-[40px] font-medium text-text-primary leading-tight">Grab Upto 50% Off On Selected Clothes</p>
         <Link href="#collections-section">
           <Button rounded={'full'} className="text-lg">
             Shop Now
